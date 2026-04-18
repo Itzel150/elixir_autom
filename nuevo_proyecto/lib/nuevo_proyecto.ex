7c1034259e2de31f609497f36149f89e9127e853
Itzelene Valladares <itzevallabrito@gmail.com>
2026-04-17 23:38:24 -0600

    Agrego proyecto Elixir

---
+++ nuevo_proyecto/lib/nuevo_proyecto.ex
@@ -0,0 +1,18 @@
+defmodule NuevoProyecto do
+  @moduledoc """
+  Documentation for `NuevoProyecto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NuevoProyecto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ nuevo_proyecto/test/nuevo_proyecto_test.exs
@@ -0,0 +1,8 @@
+defmodule NuevoProyectoTest do
+  use ExUnit.Case
+  doctest NuevoProyecto
+
+  test "greets the world" do
+    assert NuevoProyecto.hello() == :world
+  end
+end
